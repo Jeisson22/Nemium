b015fbfe37ca075bb19a02beed72a831d0d6a4f5
Jeisson22 <jeisson.15.ga@gmail.com>
2025-09-02 23:40:07 -0500

    Reproductor

---
+++ js/app.js
@@ -0,0 +1,14 @@
+const  $video = document.querySelector('#video');
+const  $play = document.querySelector('#play');
+const  $pause = document.querySelector('#pause');
+
+
+$play.addEventListener('click',handlePlay);
+
+
+function handlePlay(){
+  console.log('play video');
+}
+
+
+
